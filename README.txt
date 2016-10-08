e79b5e5a9bf107850bf81cf830fec91cb8fa4153
Munir <ingeniousweb.info@gmail.com>
2016-10-08 17:16:11 +0500

    Release
    Initial Release

---
+++ README.txt
@@ -0,0 +1,39 @@
+=== Plugin Name ===
+Contributors: munirkamal
+Donate link: http://ingenious-web.com/
+divi, elementor, particles-js, background
+Requires at least: 4.5
+Tested up to: 4.6.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Let\'s add some interactive background to the sections in Divi or Elementor easily using particles.js
+
+== Description ==
+
+
+Let\'s add some interactive background to the sections in Divi or Elementor easily using particles.js
+
+== Installation ==
+
+Just upload and activate. Done!
+
+e.g.
+
+1. Upload `plugin-name.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Place `<?php do_action('plugin_name_hook'); ?>` in your templates
+
+== Screenshots ==
+
+1. This screen shot description corresponds to screenshot-1.(png|jpg|jpeg|gif). Note that the screenshot is taken from
+the /assets directory or the directory that contains the stable readme.txt (tags or trunk). Screenshots in the /assets
+directory take precedence. For example, `/assets/screenshot-1.png` would win over `/tags/4.3/screenshot-1.png`
+(or jpg, jpeg, gif).
+2. This is the second screen shot
+
+== Changelog ==
+
+= 1.0 =
+* Released
---
+++ particlejs.php
@@ -0,0 +1,76 @@
+<?php 
+/*
+Plugin Name: ParticleJS Background
+Plugin URI: http://cakewp.com/
+Description: TLet\'s add some interactive background to the sections in Divi or Elementor easily using particles.js
+Author: Munir Kamal
+Version: 1.0
+Author URI: http://ingenious-web.com/
+*/
+
+//Load ParticleJs file
+add_action( 'wp_enqueue_scripts', 'iw_particleJs_enqueue_script' );
+function iw_particleJs_enqueue_script() {
+  wp_enqueue_script( 'iw_particleJs', plugin_dir_url( __FILE__ ) . '/includes/particles.js', false );
+}
+
+/**
+ * custom option and settings
+ */
+//Admin Menu
+add_action("admin_menu", "add_iw_particleJs_menu_item");
+function add_iw_particleJs_menu_item()
+{
+    add_menu_page("Particle Background", "Particle Background", "manage_options", "iwParticleJs", "iw_particleJs_settings_page", null, 99);
+}
+
+function iw_particleJs_settings_page() {
+  ?>
+    <div class="wrap">
+    <h2>Interactive Background Plugin by Munir Kamal</h2>
+
+    <form method="post" action="options.php">
+        <?php settings_fields( 'iw_particleJs_group' ); ?>
+        <?php do_settings_sections( 'iw_particleJs_group' ); ?>
+        <table class="form-table">
+            <tr valign="top">
+            <th scope="row">Add Your Json Code here</th>
+            <td>
+            <textarea id='particlejs' name='particlejs' rows='20' cols='50' type='textarea'><?php echo esc_attr( get_option('particlejs') ); ?></textarea>
+            </td>
+            </tr>
+        </table>
+        
+        <?php submit_button(); ?>
+
+    </form>
+    </div>
+  <?php
+}
+
+//Admin Settings
+add_action( 'admin_init', 'iw_particleJs_settings' );
+function iw_particleJs_settings() {
+    register_setting( 'iw_particleJs_group', 'particlejs' );
+}
+
+/**
+ * Output the saved json code
+ */
+
+function output_iw_particleJs() {
+    ?>
+        <script type="text/javascript">
+           <?php echo get_option('particlejs'); ?>
+        </script>
+
+        <style type="text/css">
+            canvas.particles-js-canvas-el {
+                position: absolute;
+                top: 0;
+                left: 0;
+            }
+        </style>
+    <?
+}
+add_action('wp_footer', 'output_iw_particleJs');
